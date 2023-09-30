59fa9dfd20b9c5215d155e8ec10a58c86682183d
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2023-10-01 00:33:35 +0200

    basic ai implementation

--- src/components/Game/Character.jsx
+++ src/components/Game/Character.jsx
@@ -2,7 +2,7 @@ export default function Character({ position, characterRef }) {
     return (
         <mesh ref={characterRef} scale={[0.25, 0.25, 0.25]} rotation={[Math.PI / 2, 0, Math.PI]} position={position} castShadow receiveShadow>
             <coneGeometry attach="geometry" args={[1, 2.5, 10]} smoothness={5} />
-            <meshStandardMaterial color="red" />
+            <meshStandardMaterial color="yellow" />
         </mesh>
     );
 }
