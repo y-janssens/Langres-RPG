ddd6984a2e50cf93d9b386da0c7e088ca57ebeaa
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2023-11-27 00:12:27 +0100

    clean unimported components and dependencies

--- src/components/ui/Close.jsx
+++
@@ -1,9 +0,0 @@
-import css from "./ui.module.css";
-
-export const Close = ({ onClick = () => {} }) => {
-  return (
-    <div className={css["close-button"]} onClick={onClick}>
-      X
-    </div>
-  );
-};
