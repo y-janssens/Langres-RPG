2b41839e8b50cd3891b279135a506296e40fc655
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2024-03-17 21:43:57 +0100

    add default languages to settings

--- src-tauri/migrations/2023-11-06-220214_application_datas/up.sql
+++ src-tauri/migrations/2023-11-06-220214_application_datas/up.sql
@@ -2,10 +2,22 @@
 CREATE TABLE "settings" (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     language TEXT NOT NULL,
+    languages TEXT NOT NULL,
     sound BOOLEAN NOT NULL,
     volume INTEGER NOT NULL,
     music INTEGER NOT NULL
 );
 
-INSERT INTO "settings"(language, sound, volume, music)
-VALUES ("en", 1, 100, 100);
+INSERT INTO "settings"(language, languages, sound, volume, music)
+VALUES ("en", json('
+[
+  {
+    "key": "en",
+    "value": "English"
+  },
+  {
+    "key": "fr",
+    "value": "Français"
+  }
+]
+'), 1, 100, 100);
