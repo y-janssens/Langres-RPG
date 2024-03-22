e1443a1b2ea3f20fa452e8ca4df997d18a169732
Yoann Janssens <74479083+y-janssens@users.noreply.github.com>
2024-03-22 22:59:21 +0100

    implement player statistics for player journal

--- src-tauri/src/player/journal/models.rs
+++ src-tauri/src/player/journal/models.rs
@@ -4,6 +4,7 @@ use serde::{Deserialize, Serialize};
 use crate::config::factory::factory_models::AbstractModel;
 use crate::player::achievements::models::PlayerAchievement;
 use crate::player::quests::models::PlayerQuest;
+use crate::player::statistics::models::PlayerStatistic;
 
 impl AbstractModel for PlayerJournal {}
 
@@ -11,15 +12,18 @@ impl AbstractModel for PlayerJournal {}
 pub struct PlayerJournal {
     pub achievements: Vec<PlayerAchievement>,
     pub quests: Vec<PlayerQuest>,
+    pub statistics: Vec<PlayerStatistic>,
 }
 
 impl PlayerJournal {
     pub fn load(_id: i32, connection: &mut SqliteConnection) -> QueryResult<PlayerJournal> {
         let achievements = PlayerAchievement::load(_id, connection)?;
         let quests = PlayerQuest::load(_id, connection)?;
+        let statistics = PlayerStatistic::load(_id, connection)?;
         let journal = PlayerJournal {
-            achievements,
             quests,
+            achievements,
+            statistics,
         };
         Ok(journal)
     }
--- src-tauri/src/player/journal/tests.rs
+++ src-tauri/src/player/journal/tests.rs
@@ -3,12 +3,13 @@ mod tests {
     use crate::achievements::models::Achievement;
     use crate::config::conf::test_conf::allow_db_access;
     use crate::config::factories::factories_definitions::{
-        AchievementFactory, GameFactory, QuestFactory,
+        AchievementFactory, GameFactory, QuestFactory, StatisticFactory,
     };
     use crate::config::factory::factory_models::{ApiFactory, Factory};
     use crate::game::models::Game;
     use crate::player::journal::models::PlayerJournal;
     use crate::quests::models::Quest;
+    use crate::statistics::models::Statistic;
 
     #[test]
     fn test_load_player_journal() {
@@ -16,8 +17,10 @@ mod tests {
             for _ in 0..25 {
                 let achievement = AchievementFactory.generate();
                 let quest = QuestFactory.generate();
+                let statistic = StatisticFactory.generate();
                 let _ = Achievement::save(achievement, connection);
                 let _ = Quest::save(quest, connection);
+                let _ = Statistic::save(statistic, connection);
             }
 
             let game = GameFactory.generate(connection);
@@ -27,6 +30,7 @@ mod tests {
 
             assert_eq!(player_journal.achievements.len(), 25);
             assert_eq!(player_journal.quests.len(), 25);
+            assert_eq!(player_journal.statistics.len(), 25);
         });
     }
 }
